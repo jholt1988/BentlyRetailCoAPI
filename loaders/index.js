1efdc0fdcaf2eda34a28b50adacd9e0662686ce4
Jordan A Holt <jordanh316@gmail.com>
2022-01-26 14:43:47 -0600

    Add ERD to project and create user Model. Continue work auth and user models, services, and endpoints

--- loaders/index.js
+++ loaders/index.js
@@ -1,16 +1,29 @@
 const expressLoader = require('./express')
-
-
+const passportLoader = require('./passport')
 const swaggerLoader = require('./swagger')
 
 module.exports = async (app) => {
     //Load express middleware
-const expressApp = await expressLoader(app)
+    const expressApp = await expressLoader(app);
+
+    const passport = await passportLoader(expressApp)
+
 
     //Load swagger middleware
     await swaggerLoader(app)
 
 
+    //Error Handling
+
+    app.use((err, req, res, next) => {
+
+        const { errorMessage, status } = err;
+
+        return res.status(status | 500).send({ errorMessage });
+        
+
+    });
+    return app;
 
 
 }
